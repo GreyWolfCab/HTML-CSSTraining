aedebfa3f483e4ab1fc4ed334ab5013e0564774c
GreyWolfCab <54644740+GreyWolfCab@users.noreply.github.com>
2020-04-14 14:40:45 -0700

    Finished Restful_API Tutorial

--- Restful_API/app.js
+++ Restful_API/app.js
@@ -6,12 +6,16 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect('mongodb+srv://nutmeg:' + process.env.MONGO_ATLAS_PW + '@internut-zy8hk.mongodb.net/test?retryWrites=true&w=majority', {
-    useMongoClient: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+mongoose.Promise = global.Promise;//solves deprecation warning
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -29,6 +33,7 @@ app.use((req, res, next) => {
 //middleware which sends requests to routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 //error handling request
 app.use((req, res, next) => {
